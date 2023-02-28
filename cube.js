5e4d0e47e6d79bc6be3ff7e4ed92023de56c8ec9
Carlos Junior Ramos Vasquez <juniorencode@outlook.com>
2023-02-28 18:33:49 -0500

    ayudando a radi xd

--- cube.js
+++ cube.js
@@ -3,54 +3,126 @@ const cube = {
     x: 160,
     y: 0,
     content: [
-      [{ color: 'white' }, { color: 'white' }, { color: 'white' }],
-      [{ color: 'white' }, { color: 'white' }, { color: 'white' }],
-      [{ color: 'white' }, { color: 'white' }, { color: 'white' }]
+      [
+        { color: 'white', x: 0, y: 0 },
+        { color: 'white', x: 1, y: 0 },
+        { color: 'white', x: 2, y: 0 }
+      ],
+      [
+        { color: 'white', x: 0, y: 1 },
+        { color: 'white', x: 1, y: 1 },
+        { color: 'white', x: 2, y: 1 }
+      ],
+      [
+        { color: 'white', x: 0, y: 2 },
+        { color: 'white', x: 1, y: 2 },
+        { color: 'white', x: 2, y: 2 }
+      ]
     ]
   },
   face2: {
     x: 0,
     y: 160,
     content: [
-      [{ color: 'orange' }, { color: 'orange' }, { color: 'orange' }],
-      [{ color: 'orange' }, { color: 'orange' }, { color: 'orange' }],
-      [{ color: 'orange' }, { color: 'orange' }, { color: 'orange' }]
+      [
+        { color: 'orange', x: 0, y: 0 },
+        { color: 'orange', x: 1, y: 0 },
+        { color: 'orange', x: 2, y: 0 }
+      ],
+      [
+        { color: 'orange', x: 0, y: 1 },
+        { color: 'orange', x: 1, y: 1 },
+        { color: 'orange', x: 2, y: 1 }
+      ],
+      [
+        { color: 'orange', x: 0, y: 2 },
+        { color: 'orange', x: 1, y: 2 },
+        { color: 'orange', x: 2, y: 2 }
+      ]
     ]
   },
   face3: {
     x: 160,
     y: 160,
     content: [
-      [{ color: 'green' }, { color: 'green' }, { color: 'green' }],
-      [{ color: 'green' }, { color: 'green' }, { color: 'green' }],
-      [{ color: 'green' }, { color: 'green' }, { color: 'green' }]
+      [
+        { color: 'green', x: 0, y: 0 },
+        { color: 'green', x: 1, y: 0 },
+        { color: 'green', x: 2, y: 0 }
+      ],
+      [
+        { color: 'green', x: 0, y: 1 },
+        { color: 'green', x: 1, y: 1 },
+        { color: 'green', x: 2, y: 1 }
+      ],
+      [
+        { color: 'green', x: 0, y: 2 },
+        { color: 'green', x: 1, y: 2 },
+        { color: 'green', x: 2, y: 2 }
+      ]
     ]
   },
   face4: {
     x: 320,
     y: 160,
     content: [
-      [{ color: 'red' }, { color: 'red' }, { color: 'red' }],
-      [{ color: 'red' }, { color: 'red' }, { color: 'red' }],
-      [{ color: 'red' }, { color: 'red' }, { color: 'red' }]
+      [
+        { color: 'red', x: 0, y: 0 },
+        { color: 'red', x: 1, y: 0 },
+        { color: 'red', x: 2, y: 0 }
+      ],
+      [
+        { color: 'red', x: 0, y: 1 },
+        { color: 'red', x: 1, y: 1 },
+        { color: 'red', x: 2, y: 1 }
+      ],
+      [
+        { color: 'red', x: 0, y: 2 },
+        { color: 'red', x: 1, y: 2 },
+        { color: 'red', x: 2, y: 2 }
+      ]
     ]
   },
   face5: {
     x: 480,
     y: 160,
     content: [
-      [{ color: 'blue' }, { color: 'blue' }, { color: 'blue' }],
-      [{ color: 'blue' }, { color: 'blue' }, { color: 'blue' }],
-      [{ color: 'blue' }, { color: 'blue' }, { color: 'blue' }]
+      [
+        { color: 'blue', x: 0, y: 0 },
+        { color: 'blue', x: 1, y: 0 },
+        { color: 'blue', x: 2, y: 0 }
+      ],
+      [
+        { color: 'blue', x: 0, y: 1 },
+        { color: 'blue', x: 1, y: 1 },
+        { color: 'blue', x: 2, y: 1 }
+      ],
+      [
+        { color: 'blue', x: 0, y: 2 },
+        { color: 'blue', x: 1, y: 2 },
+        { color: 'blue', x: 2, y: 2 }
+      ]
     ]
   },
   face6: {
     x: 160,
     y: 320,
     content: [
-      [{ color: 'yellow' }, { color: 'yellow' }, { color: 'yellow' }],
-      [{ color: 'yellow' }, { color: 'yellow' }, { color: 'yellow' }],
-      [{ color: 'yellow' }, { color: 'yellow' }, { color: 'yellow' }]
+      [
+        { color: 'yellow', x: 0, y: 0 },
+        { color: 'yellow', x: 1, y: 0 },
+        { color: 'yellow', x: 2, y: 0 }
+      ],
+      [
+        { color: 'yellow', x: 0, y: 1 },
+        { color: 'yellow', x: 1, y: 1 },
+        { color: 'yellow', x: 2, y: 1 }
+      ],
+      [
+        { color: 'yellow', x: 0, y: 2 },
+        { color: 'yellow', x: 1, y: 2 },
+        { color: 'yellow', x: 2, y: 2 }
+      ]
     ]
   }
 };
